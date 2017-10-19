ed6ba53363a24ed35f885a036877092731b31107
AsTaEbot <31821044+AsTaEbot@users.noreply.github.com>
2017-10-20 00:06:36 +0330

    Add files via upload

---
+++ help.lua
@@ -0,0 +1,880 @@
+do local _ = {
+helpfa = {
+help = [[
+» `لیست بخش های راهنمای ربات` :
+
+•• `لطفا یک بخش را برای نمایش انتخاب کنید` !
+
+`1` . `فعال یا غیرفعال سازی وضعیت های ربات`
+
+`2` . `فعال یا غیرفعال سازی قفل های اصلی`
+
+`3` . `فعال یا غیرفعال سازی قفل های رسانه`
+
+`4` . `تغییر وضعیت و حساسیت قفل فلود و اسپم`
+
+`5` . `بخش مربوط به ارتقا و عزل مقام یک کاربر`
+
+`6` . `تنظیم و دریافت لینک گروه`
+
+`7` .`امکانات جانبی ربات`
+
+`8` . `تنظیمات و پنل ها`
+
+`9` . `روش پاکسازی لیست ها`
+
+`10` . `حذف و دادن اخطار`
+
+`11` . `بخش دستورات تفریحی و عمومی`
+
+`0` . `لغو عملیات`
+
+> `برای دریافت راهنمای بخش مورد نظر کافیست شماره آن را ارسال نمایید` !
+
+>`کانال ما` | @SShteam
+]]
+,
+helplock = [[
+••  راهنمای قفل های اصلی :
+
+شما میتوانید قفل های ربات را با دستور ( قفل [ نوع ] ) فعال و با دستور ( بازکردن [ نوع ] ) غیرفعال نمایید !
+
+به طور مثال :
+`قفل لینک`
+`بازکردن لینک`
+
+> تمامی قفل های اصلی ربات :
+
+- `اسپم`
+- `لینک`
+- `فلود`
+- `صفحات اینترنتی`
+- `سرویس تلگرام`
+-  `نوشتار فارسی`
+- `نوشتار انگلیسی`
+- `دکمه شیشه ای`
+- `تگ`
+- `هشتگ`
+- `ورود ربات`
+- `فروارد`
+- `ویرایش پیام`
+- `مدل نشانه گذاری`
+- `پست`
+- `پست تکراری`
+- `بازی`
+- `سنجاق پیام`
+- `ورود عضو`
+    ‌‌
+]]
+,
+helpmedia = [[
+•• راهنمای قفل های رسانه :
+
+شما میتوانید قفل های ربات را با دستور ( قفل [ نوع ] ) فعال و با دستور ( بازکردن [ نوع ] ) غیرفعال نمایید !
+
+به طور مثال :
+`قفل فیلم`
+`بازکردن فیلم`
+
+> تمامی قفل های رسانه ربات :
+
+- `گیف`
+- `ویس`
+- `موزیک`
+- `فایل`
+- `فیلم`
+- `فیلم سلفی`
+-  `عکس`
+- `متن`
+- `استیکر`
+- `موقعیت مکانی`
+]]
+,
+helpsetlink = [[
+•• راهنمای تنظیم و دریافت لینک گروه :
+
+ابتدا دستور  ( `تنظیم لینک`) را وارد کنید !
+
+سپس لینک گروه را داخل خود گروه ارسال کنید تا ثبت شود !
+
+پس از ثبت شدن میتوانید با دستور `لینک گروه` آن را هر زمان که خواستید دریافت کنید !
+    ‌‌‌‌
+]]
+,
+helpprodemo = [[
+•• راهنمای مدیران برای ارتقا یا عزل مقام یک کاربر :
+
+• برای ارتقای مقام چهار روش وجود دارد !
+
+> روش اول : 
+
+روی یکی از پیام های کاربر مورد نظر ریپلای کنید و دستور (` ارتقا مقام `) را ارسال کنید !
+
+> روش دوم : 
+
+دستور ( `ارتقا مقام `) را بنویسید و جلوی دستور آیدی عددی فرق مورد نظر را بنویسید !
+
+مثال :
+
+`ارتقا مقام 228214123`
+
+> روش سوم :
+
+دستور (` ارتقا مقام `) را بنویسید و جلوی آن یوزرنیم کاربر مورد نظر را بنویسید !
+
+مثال :
+
+`ارتقا مقام @Username`
+
+• برای عزل مقام یک نفر کافیست مراحل بالا را انجام دهید تنها با تفاوت اینکه به جای دستور "`ارتقا مقام`" دستور "`عزل مقام`" را جایگزین کنید !
+ 
+مثال :
+
+`عزل مقام @Username`
+    ‌‌‌‌
+> روش چهارم :
+
+    ‌‌
+این روش با منشن انجام میشود و میتوانید پروموت کنید
+
+`ارتقا مقام "منشن"`
+    ‌‌
+]]
+,
+helpjanebi = [[
+•• راهنمای امکانات جانبی ربات :
+
+برای دریافت مشخصات خود میتوانید از دستور ( `آیدی` ) استفاده کنید !
+
+اگر میخواهید ربات مشخصات شما را همراه با عکس پروفایل شما ارسال کند ، دستور (` وضعیت نمایش آیدی عکس` ) را ارسال کنید ! برای بازگشت به حالت ساده دستور (` وضعیت نمایش آیدی ساده `) را ارسال نمایید !
+
+دستور( `اطلاعات من` ) برای دریافت شناسه عددی و مقام شما میباشد !
+
+برای دریافت شناسه عددی یک فرد میتوانید از 2 روش استفاده کنید
+
+> روش اول : 
+
+ریپلای کردن یک پیام فرد مورد نظر و ارسال دستور  (` آیدی `)
+
+> روش دوم :
+
+نوشتن دستور id و جلوی آن گذاشتن یوزرنیم فرد مورد نظر
+مثال :
+`آیدی @Username`
+
+برای دریافت عکس های پروفایل خود میتوانید از دستور زیر استفاده کنید
+
+`عکس پروفایلم` [ 1 - 10 ]
+
+مثال : 
+`عکس پروفایلم 2`
+
+برای سنجاق کردن یک پیام توسط ربات میتوانید پیام مورد نظر را ریپلای کنید سپس دستور (`سنجاق کن`) را ارسال کنید
+برای خارج کردن پیام از حالت سنجاق میتوانید از دستور ( `حذف سنجاق` ) استفاده کنید
+برای مجدد سنجاق کردن پیام سابق میتوانید از دستور ( `سنجاق مجدد`) استفاده نمایید
+    ‌‌‌‌
+]]
+,
+helpspamflood = [[
+•• راهنمای قفل اسپم و فلود ربات :
+
+قفل اسپم` از ارسال پیام های بلند و طولانی جلوگیری میکند` !
+حساسیت آن قابل تنظیم است و واحد آن کاراکتر ( `حرف` ) میباشد !
+دستور تنظیم آن به طور زیر میباشد :
+
+تنظیم اسپم `[ 40 - به بالا ]`
+
+مثلا شما میخواهید پیام های طولانی تر از 80 حرف پاک بشوند باید از دستور ( `تنظیم اسپم 80`) استفاده نمایید !
+
+قفل فلود` از ارسال چندین پیام پشت سر هم جلوگیری میکند` !
+حساسیت و وضعیت آن قابل تنظیم است !
+
+وضعیت آن 2 حالت میباشد !
+
+> حالت اول : 
+
+اگر کسی شروع به ارسال پیام پشت سر هم بکند ، تمامی پیام های او پاک خواهد شد و تا 5 ثانیه نمیتواند پیامی ارسال کند !
+
+دستور فعال سازی این وضعیت :
+
+`وضعیت فلود حذف پیام`
+
+> `حالت دوم` : 
+
+اگر کسی شروع به ارسال پیام پشت سر هم بکند ، تمامی پیام های او پاک خواهد شد و از گروه هم ریموو خواهد شد !
+
+دستور فعال سازی این وضعیت :
+
+`وضعیت فلود اخراج`
+
+حساسیت آن هم به حد پیام تکراری قابل تنظیم است !
+
+تنظیم فلود `[ 1 - به بالا ]`
+    ‌‌‌‌
+]]
+,
+helpvaziat = [[
+•• راهنمای وضعیت های ربات :
+
+> حالت سختگیرانه
+
+حالتی است که اگر فعال باشد ، کسی که لینک ارسال کند را از گروه ریموو میکند !
+
+فعال سازی :
+
+`قفل حالت سختگیرانه`
+
+غیرفعال سازی :
+
+`بازکردن حالت سختگیرانه`
+
+> حالت قفل کلی گروه : 
+
+حالتی است که از ارسال هر گونه پیام توسط کاربران عادی جلوگیری میکند !
+
+فعال سازی :
+
+`قفل گروه`
+
+غیرفعال سازی :
+
+`بازکردن گروه`
+
+برای فعال سازی به صورت زمان دار میتوانید به شکل زیر عمل کنید :
+
+فعال سازی برای مدت یک ساعت :
+
+`قفل جی تایم 1`
+
+فعال سازی به مدت دو ساعت :
+
+`قفل جی تایم 2 0 0`
+
+و الی آخر ...
+
+> حالت عدم جواب : 
+
+بعضی از دستورات همگانی هستند ، یعنی به کاربر عادی هم پاسخ داده میشود ، اگر این حالت فعال باشد به آنها پاسخ داده نخواهد شد ! نخواهد داد !
+
+فعال سازی :
+
+`قفل حالت عدم جواب`
+
+غیرفعال سازی :
+
+`بازکردن حالت عدم جواب`
+    ‌‌‌‌
+]]
+,
+helppanel = [[
+•• راهنماي پنل گروه :
+
+> فعال کردن نمایش شماره تلفن
+
+`وضعیت نمایش شماره تلفن فعال`
+
+> غيرفعال کردن افزودن شماره
+
+`وضعیت نمایش شماره تلفن غیرفعال`
+
+> تغيير زبان گروه به انگليسي
+
+`تنظیم زبان انگلیسی`
+
+> تغيير زبان گروه به فارسي
+
+`تنظیم زبان فارسی`
+
+> بازگزداني تنظيمات گروه به حالت اول
+
+`تنظیم مجدد`
+
+> نمايش پيام هاي اديت شده
+
+`نمایش ادیت فعال`
+
+> نمايش ندادن پيام هاي اديت شده
+
+`نمایش ادیت غیرفعال`
+    ‌‌‌‌
+]]
+,
+helpclean = [[
+• راهنماي پاکسازي ليست ها :
+
+> برای پاک کردن دليت اکانتي ها :
+
+`پاکسازی دلیت اکانت ها`
+
+> برای پاک کردن فيلتر ليست :
+
+`پاکسازی فیلترلیست`
+
+> برای پاک کردن مديران گروه :
+
+`پاکسازی لیست مدیران گروه`
+
+> برای پاک کردن اعضای مسدود : 
+
+`پاکسازی لیست افراد مسدود`
+
+> برای پاک کردن افراد بيصدا :
+
+`پاکسازی لیست افراد بی صدا`
+
+> برای پاک کردن بلاک ليست گروه :
+
+`پاکسازی مسدودیت گروه`
+
+> برای پاک کردن تمام پيام هاي گروه :
+
+`پاکسازی پیام ها`
+]]
+,
+helpwarn = [[
+• راهنماي اخطار های روبات :
+
+> ارسال اخطار :
+
+`Warn @Username`
+
+شما براي اخطار دادن به کاربران ميتوانيد از [ ریپلای ، شناسه ] هم استفاده کنید !
+
+
+> حذف اخطار های کاربر :
+
+`Unwarn @Username`
+
+شما براي  حذف اخطار های کاربر ميتوانيد از [ ریپلای ، شناسه ] هم استفاده کنید !
+
+
+> تنظيم تعداد دفعات اخطار به کاربر :
+
+`Setwarn` [ 1 - Up ]
+> تنظيم مجازات :
+
+• بی صدا شدن در صورت اتمام دفعات اخطار
+
+`وضعیت اخطار بی صدا`
+
+• اخراج شدن در صورت اتمام دفعات اخطار
+
+`وضعیت اخطار اخراج`
+]]
+,
+helpfun = [[
+• راهنمای دستورات تفریحی و عمومی :
+
+<اوقات شرعي :
+
+•• با استفاده از اين دستور شما مي‌توانيد اوقات شرعي شهر وارد شده را دريافت كنيد.
+
+دستور فارسي :
+`اوقات شرعي` ( `اسم شهر` )
+
+نمونه دستور فارسي :
+`اوقات شرعي تهران`
+
+>تاريخ و زمان :
+
+•• با استفاده از اين دستور شما مي‌توانيد از زمان و تاريخ جاري مطلع شويد.
+
+دستور فارسی :
+`زمان`
+
+>تبديل استيكر به عكس :
+
+•• با استفاده از اين دستور شما مي‌توانيد استیکر خود را به تصویر تبدیل کنید.
+
+دستور فارسی :
+`تبدیل به عکس` ( `ریپلای` )
+
+>تبديل عكس به استيكر :
+
+•• با استفاده از اين دستور شما مي‌توانيد تصویر خود را به استیکر تبدیل کنید.
+
+دستور فارسی :
+`تبدیل به استیکر` ( `ریپلای` )
+
+>تبديل متن به تصوير متحرك (‌ `گيف` ) :
+
+•• با استفاده از اين دستور شما مي‌توانيد متن خود را به یک فایل تصویری متحرگ یا همان Gif تبدیل کنید.
+
+دستور فارسی :
+ساخت گیف ( `متن` ) 
+
+>استیکر عشق ( `Love` ) :
+
+•• با استفاده از این دستور شما میتوانید یک استیکر حاوی دو اسم و یک قلب ایجاد کنید ! این دستور از زبان فارسی پشتیبانی نمی‌کند و شما باید اسم ها را به زبان انگلیسی وارد کنید !
+
+دستور انگلیسی :
+Love اسم اول ، اسم دوم
+
+نمونه دستور انگليسي :
+Love Eset Robot
+
+>استیکر آروم باش ( `KeepCalm` ) :
+
+•• با استفاده از این دستور شما میتوانید یک استیکر `KeepCalm` با متن دلخواه ایجاد کنید ! این دستور نیز  همانند استیکر `Love` از زبان فارسی پشتیبانی نمی‌کند و شما باید آن را به صورت انگلیسی وارد کنید.
+
+دستور انگلیسی :
+`Keepcalm` اسم ، رنگ زمینه ، رنگ نوشته ، رنگ تاج ، اندازه
+
+نمونه دستور انگليسي :
+`keepcalm Robot white black black 600`
+
+>شناسه من :
+
+•• با استفاده از این دستور شما میتوانید آیدی عددی حساب کاربری تلگرام خود را دریافت کنید. پاسخ دهی این دستور برای کاربران عادی مبنی بر غیرفعال بودن حالت عدم جواب می‌باشد.
+
+دستور فارسی :
+آیدی
+
+دستور انگلیسی :
+id
+
+>مقام من :
+
+•• با استفاده از این دستور شما میتوانید اطلاعات اکانت خود ( `نام اکانت ، یوزر نیم و آیدی عددی` ) و همچنین مقام خود نزد ربات ( `کاربر / عضو ویژه / مدیر گروه / صاحب گروه` ) را دریافت کنید. پاسخ دهی این دستور برای کاربران عادی مبنی بر غیرفعال بودن حالت عدم جواب می‌باشد.
+
+دستور فارسی :
+اطلاعات من
+
+دستور انگلیسی :
+Me
+]]
+}
+,
+helpen = {
+help = [[
+»* List of bot Guide *:
+
+•• Please choose a * Section * to display!
+
+`1-` Enable or disable the status of the Robot
+`2-` Lock the main section to enable or disable
+`3-` Lock or unlock the media lock
+`4-` Switch and lock sensitivity Flood and spam
+`5-` Promotion and dismissal of the user
+`6-` Part of the set and get link Group
+`7-` Ancillary facilities robot
+`8-` Set Settings of Group Panel
+`9-` How to Clean lists
+`10-` Section Warnings
+`11-` Fun Capability & Public Commands Guide
+`0-` Cancel operation
+
+> To get to the * FAQ * section, simply send its * Number *!
+]]
+,
+helplock = [[
+* The main lock Guide *:
+
+You can active bot locks withe `lock` Pattern and You can inactive bot locks withe` Unlock` Pattern!
+
+* Lock Example *:
+`Lock links`
+
+* Unlock Example *:
+`Unlock links`
+
+> All * Values * of Bot:
+
+- `Spam`
+- `links`
+- `Webpage`
+- `Flood`
+- `Tgservice`
+- `English`
+- `Farsi`
+- `Inline`
+- `Pin`
+- `Mention`
+- `Markdown`
+- `Edit`
+- `Bots`
+- `Fwd`
+- `Hashtag`
+- `Post`
+- `Duplipost`
+- `Game`
+- `Inline`
+- `Member`
+]]
+,
+helpmedia = [[
+•• * The main Media Guide *:
+
+You can active Media lock withe `lock` Pattern and You can inactive Media lock withe` Unlock` Pattern!
+
+* Lock Example *:
+`Lock File`
+
+* Unlock Example *:
+`Unlock File`
+
+> All * Media * of Bot:
+
+- `Gif`
+- `Photo`
+- `Video`
+- `Selfvideo`
+- `Text`
+- `Voice`
+- `Music`
+- `location`
+- `File`
+- `Contact`
+- `Sticker`
+]]
+,
+helpsetlink = [[
+•• *Instruction Set and get links Group* :
+
+Enter the command `Setlink`  !
+
+Then link the group to be *Recorded* within the group itSelf  !
+
+Once registered with the command `Link` can get it whenever you want !
+]]
+,
+helpprodemo = [[
+•• * A Manager's Guide to Promotion or dismissal as a user *:
+
+>> There are Four ways for *Promotion*!
+
+> The first Method:
+
+One of the messages to the user and command Ryplay *Promote submit*!
+
+> The second method:
+
+Promote Write command and the command to write the same numeric ID!
+* Example *:
+`Promote 201321014`
+
+> The third method:
+
+Promote ordered to write and record the user's username in front of it!
+* Example *:
+`Promote @Username`
+
+Dismissal of the above steps is enough for one person only with the difference that instead ordered to replace Promote Demote command
+* Example *:
+`Demote @Username`
+
+> The four method:
+
+* Example *
+
+`Promote 'Metion'`
+]]
+,
+helpjanebi = [[
+•• *Lateral guide features* :
+
+Id command you can use to *get your profile* !
+
+If you want your details along with your profile photo robot sends orders to send `Getidstatus photo` ! To return to the simple mode command to send `Getidstatus simple` !
+
+Ordered `me` to get the numeric ID is your place!
+
+To get a numeric identifier, you can use two methods
+
+> The first method :
+
+Ryplay person a message and send commands `Id`
+
+> The second Method :
+
+Id command and the *Username* of the Person writing it up
+*Example* :
+`id @Username`
+
+To get to see your profile, you can use the following command
+
+`getpro [1 - 10]`
+
+*Example* :
+`getpro 2`
+
+To pin down a message via robot can send messages to the `Pin` Ryplay then ordered !
+To remove the message from the pin `Unpin` can use the command !
+To re-pin the former message, you can use the command `Repin` !
+]]
+,
+helpspamflood = [[
+•• *Guide lock flood and spam* :
+
+Lock will prevent spammers from sending messages loud and long!
+Its sensitivity is adjustable and unit characters is!
+It is set in the following order:
+
+`Setspam [40 - Up]`
+
+For example, you want to delete messages longer than 80 characters should be used from the command Setspam 80!
+
+Flood lock prevents the burst, sending multiple messages!
+Sensitivity is adjustable and it shows!
+
+It is the second mode!
+
+> First :
+
+If he burst start *Sending messages*, all messages will be removed and 5 seconds he can send a message!
+
+Active command of the Situation :
+
+`Setstatus del`
+
+> The second mode :
+
+If he burst start *Sending messages* , all messages will be deleted from the group Remove he will !
+
+`Setstatus kick`
+
+Its *Sensitivity* is adjustable to a duplicate message!
+
+`Setflood [1 - Up]`
+]]
+,
+helpvaziat = [[
+•• *Guide robot status* :
+
+> *Strict mode*
+
+A condition that, if enabled, someone from the group remove send the link!
+
+*Activation* :
+`Lock strict`
+
+*Deactivate* :
+`Unlock strict`
+
+> *Overall lock Group*
+
+A condition that prevents a normal user from sending any messages!
+
+*Activation* :
+`Lock all`
+
+*Deactivate* :
+`Unlock all`
+
+To enable the timer can act as follows:
+
+*Activation for an Hour* :
+`lock gtime 1`
+
+*Activation for two Hours* :
+`lock gtime 2 0 0`
+
+And ...
+
+> *NoN-answer*
+
+Some of universal grammar, that is normal users are answered, they will not respond if this mode is enabled!
+
+*Activation* :
+`Lock cmd`
+
+*Deactivate* :
+`Unlock cmd`
+]]
+,
+helppanel = [[
+•• *Guide Group Panel* :
+
+> *Enable AddPhone*
+
+Pattern :
+`addphone on`
+
+> *Disable AddPhone*
+
+Pattern :
+`addphone off`
+
+> *Set Group En language*
+
+Pattern :
+`setlang en`
+
+> *Set Group Fa language*
+
+Pattern :
+`setlang fa`
+
+> *Reset Group Status*
+
+Pattern :
+`reset`
+
+> *Show Edited Msgs*
+
+Pattern :
+`showedit on`
+
+> *UnShow Edited Msgs*
+
+Pattern :
+`showedit off`
+]]
+,
+helpclean = [[
+•• * Guide for Cleans *:
+
+> * Clean Delted Accounts *
+
+Pattern:
+`Clean deleted`
+
+> * Clean Filter Words *
+
+Pattern:
+`Clean filterlist`
+
+> * Clean Modrators list *
+
+Pattern:
+`Clean modlist`
+
+> * Clean BanUsers *
+
+Pattern:
+`Clean banlist`
+
+> * Clean MutedUsers *
+
+Pattern:
+`Clean mutelist`
+
+> * Clean Blacklist *
+
+Pattern:
+`Clean blocked`
+
+> * Clean All Pms *
+
+Pattern:
+`Clean msgs`
+
+> * Clean All Members *
+
+Pattern:
+`Clean members`
+]]
+,
+helpwarn = [[
+•• * Guide Group warns *:
+
+
+> * Send Warn *
+
+you can send warn to users withe sub Pattern and you can send it withe reply
+
+* Pattern *:
+`Warn @Username`
+
+
+> * Get unWarn *
+
+you can send unwarn to users withe sub Pattern and you can send it withe reply
+
+* Pattern *:
+`Unwarn @Username`
+
+
+> * Set Group warns *
+
+* Pattern *:
+`Setwarn [Value]`
+
+
+> * Punishments of Warnings *:
+
+- Mute user befor Warnings
+* Pattern *:
+`Warnstatus mute`
+
+- Remove user befor Warnings
+* Pattern *:
+`Warnstatus remove`
+]]
+,
+helpfun = [[
+•• * Fun Capability & Public Commands Guide *:
+
+> * Religious times *
+
+•• * With this command, you can get the legal entry of the entered city *.
+
+* Pattern *:
+`Praytime` ( `CityName` )
+
+> * Date And Time *
+
+ •• * With this command you may know the time and date *
+
+* Pattern *:
+`Time`
+
+> * Convert Photo To Sticker *
+
+•• * With this command, you can turn your images to stickers *
+
+* Pattern *:
+`To Sticker` ( `On Reply` )
+
+> * Convert Sticker To Photo *
+
+•• * With this command, you can turn your sticker to images *
+
+* Pattern *:
+`To Photo` ( `On Reply` )
+
+> * Convert Text To Gif *
+
+•• * With this command, you can turn your text to the gif *
+
+* Pattern *:
+`Gif` ( `Text` )
+
+> * KeepCalm Sticker *
+
+••  * With this command, you can make keepcalm stickers with Custom text . This command doesn't support in the persian language just as Love Sticker and you must type it English *
+
+* English Command * :
+`Keepcalm Name , BackGround Color , Text Color , Crown Color , Size`
+
+* Pattern *:
+`keepcalm Robot white black black 600`
+
+> * Love Sticker *
+
+•• * With this command, you can make a sticker contains two names and one heart. This command doesnt support persian language the names in English *
+
+* English Command * :
+`Love First Name , Second Name`
+
+* Pattern *:
+`Love Eset Robot`
+
+> * My Id Command *
+
+•• * With this command, you can get your Numeric Telegram ID. Answering to this command for the normall users depends on disabeling Non-response mode *
+
+* Pattern *:
+`id`
+
+> * My Rank Command *
+
+•• * With this command, you can get your Account Informations ( Numeric Telegram ID , UserName , Account FirstName ) And Your Rank In Robot ; Answering to this command for the normall users depends on disabeling Non-response mode *
+
+* Pattern *:
+`Me`
+]]
+}
+}
+return _
+end
